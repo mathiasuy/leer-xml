dc89aec430cfde6cd31218e7124628ebbe334aed
Mathias Battistella <mathias2b@gmail.com>
2018-07-10 05:20:29 +0100

    fin

---
+++ LeerXML/LeerXML.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LeerXML.aspx.cs" Inherits="LeerXML.LeerXML" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Message" runat="server"></asp:Label>
+        <br />
+        <br />
+     <asp:Button ID="bt_crear_XML" runat="server" Text="Crear XML" onClick="bt_crear_XML_Click"/>
+        <br />
+        <br />
+        <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+        <asp:Button ID="btnBuscar" runat="server" onclick="btnBuscar_Click" 
+            Text="Buscar" />
+        <asp:Button ID="btnEliminar" runat="server" onclick="btnEliminar_Click" 
+            Text="Eliminar" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ LeerXML/LeerXML.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Xml.Linq;
+
+namespace LeerXML
+{
+    public partial class LeerXML : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            buscarEnXML(txtId.Text);
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bt_crear_XML_Click(object sender, EventArgs e)
+        {
+            //crear_XML();
+        }
+
+
+        private void buscarEnXML(string idempleado)
+        {
+            XDocument miXML = XDocument.Load(@"D:\\Documentos\\Visual Studio 2010\\Projects\\LeerXML\\Sob_219999830019_20150423_1.xml"); //Cargar el documento
+
+            XNamespace nsy = "http://cfe.dgi.gub.uy";
+            XNamespace ns0 = "http://cfe.dgi.gub.uy";
+
+            var nombreusu = from xml in miXML.Elements(nsy + "EnvioCFE_entreEmpresas").Elements(nsy + "CFE_Adenda").Elements(ns0 + "CFE")
+                            select xml.Elements(ns0 + "eFact").Select(a => a.Value).ToList(); //Seleccionamos el nombre
+
+            foreach (var minom in nombreusu)
+            {
+                Message.Text = minom.Count.ToString(); //Mostramos un mensaje con el nombre del empleado que corresponde
+            }
+        }
+    }
+}
